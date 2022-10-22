244b6077a77855b98920160e3e4679eb24f79b4d
raja <rajagithub18@gmail.com>
2022-10-22 16:24:47 +0530

    added shell fike

---
+++ task.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+az group create -l eastus -n activity1 
+az network vnet create -g activity1 -n net1 --address-prefixes 10.0.0.0/16 --location eastus
+az network vnet subnet create --name web --resource-group activity1  --vnet-name net1 --address-prefixes 10.0.0.0/24
+az network vnet subnet create --name app --resource-group activity1 --vnet-name net1 --address-prefixes 10.0.1.0/24
+az network vnet subnet create --name db --resource-group activity1 --vnet-name net1 --address-prefixes 10.0.2.0/24
+az network nsg create --name nsg1 -g activity1 -l eastus 
+az network nsg rule create --name appnsg --nsg-name nsg1 --resource-group activity1 --priority 500 --access Allow --source-port-ranges 22 --protocol Tcp --direction Inbound --source-address-prefixes * 
+az network nsg rule create --name dbnsg --nsg-name nsg1 --resource-group activity1 --priority 510 --access Allow --source-port-ranges 80  --protocol Tcp --direction Inbound --source-address-prefixes * 
+az vm create --name vm1 -g activity1 --image UbuntuLTS --vnet-name net1 --subnet web --size Standard_B1s --public-ip-address vmpub --public-ip-sku Standard --public-ip-address-allocation dynamic --public-ip-sku Standard --nsg nsg1 --location eastus --admin-username devops --admin-password Motherindia@123 --authentication-type password
